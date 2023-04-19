e4a6c6e4c43188e61aa7bfd357ecb059edab5425
JnathanCarter <jcarter20@stu.jsu.edu>
2023-04-19 10:34:11 -0500

    Add Packer BootStrap

--- lua/jonathan/packer.lua
+++ lua/jonathan/packer.lua
@@ -2,8 +2,8 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
-
+--vim.cmd [[packadd packer.nvim]]
+--[[
 return require('packer').startup(function(use)
 	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'
@@ -60,4 +60,90 @@ use {
 }
 }
 
+end)
+
+--]]
+
+
+---------------- NEW With BootStrap
+--
+--
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+		vim.cmd [[packadd packer.nvim]]
+		return true
+	end
+	return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+	use 'wbthomason/packer.nvim'
+	-- My plugins here
+	-- use 'foo1/bar1.nvim'
+	-- use 'foo2/bar2.nvim'
+
+
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+
+	}
+
+	use { "bluz71/vim-moonfly-colors", as = "moonfly" }
+	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+	use('nvim-treesitter/playground')
+	use('ThePrimeagen/harpoon')
+	use('mbbill/undotree')
+	use('tpope/vim-fugitive')
+	use 'mfussenegger/nvim-jdtls'
+	use { 'alexghergh/nvim-tmux-navigation', config = function()
+
+		local nvim_tmux_nav = require('nvim-tmux-navigation')
+
+		nvim_tmux_nav.setup {
+			disable_when_zoomed = true -- defaults to false
+		}
+
+		vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+		vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+		vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+		vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+		vim.keymap.set('n', "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+		vim.keymap.set('n', "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+
+	end
+	}
+use {
+	'VonHeikemen/lsp-zero.nvim',
+	branch = 'v2.x',
+	requires = {
+		-- LSP Support
+		{'neovim/nvim-lspconfig'},             -- Required
+		{                                      -- Optional
+		'williamboman/mason.nvim',
+		run = function()
+			pcall(vim.cmd, 'MasonUpdate')
+		end,
+	},
+	{'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+	-- Autocompletion
+	{'hrsh7th/nvim-cmp'},     -- Required
+	{'hrsh7th/cmp-nvim-lsp'}, -- Required
+	{'L3MON4D3/LuaSnip'},     -- Required
+}
+}
+
+
+-- Automatically set up your configuration after cloning packer.nvim
+-- Put this at the end after all plugins
+if packer_bootstrap then
+	require('packer').sync()
+end
 end)
